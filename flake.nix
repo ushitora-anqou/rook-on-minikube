c78e3b1b50a8884b8603c7fd7f0744b2d1dafb7d
Ryotaro Banno <ryotaro.banno@gmail.com>
2025-11-28 00:29:19 +0000

    add flake.nix for reproducibility

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "rook-ceph-dev";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+  in {
+    formatter."${system}" = pkgs.alejandra;
+
+    devShells."${system}".default = pkgs.mkShell {
+      packages = with pkgs;
+        [
+          minikube
+          kubernetes-helm
+        ];
+    };
+  };
+}
